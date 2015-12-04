80ef08113e7b317de6839dc33efcbb07bd2946a8
Theodore Rogers <theodore@acrobatiq.com>
2015-12-04 18:40:51 -0500

    whitelisted table attributes

--- src/textAngular-sanitize.js
+++ src/textAngular-sanitize.js
@@ -555,7 +555,7 @@ function validCustomTag(tag, attrs, lkey, value){
       return true;
     }
     if (tag === 'span'){
-        if(lkey.startsWith('data-aq') || lkey === 'activityLink' || lkey === 'data-asset-id' || lkey === 'aq-tag-icon' || lkey === 'entry' || lkey === 'id' || lkey === 'comment' || lkey === 'term' || lkey === 'annotation') {
+        if(lkey.startsWith('data-aq') || lkey === 'activityLink' || lkey === 'data-asset-id' || lkey === 'aq-tag-icon' || lkey === 'entry' || lkey === 'id' || lkey === 'comment' || lkey === 'table' || lkey === 'table-html' || lkey === 'term') {
           return true;
         }
     }
